20acb6411a17a3615b64b7e3d576d00094da5854
scoopapa <pccompositor@aim.com>
2019-11-11 20:34:28 -0600

    Roulettemons
    87109873445675212401

---
+++ data/mods/roulettemons/formats-data.js
@@ -0,0 +1,142 @@
+'use strict';
+
+exports.BattleFormatsData = {
+	koatric: {
+		tier: "OU",
+		doublesTier: "DOU",
+	aquazelle: {
+		tier: "OU",
+		doublesTier: "DOU",
+	salamalix: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	brawnkey: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	stuneleon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	chillyte: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	eartharoo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	crazefly: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	electritar: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	aquatopus: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	scorpita: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	baloon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	kinesel: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	glacida: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pidgeotine: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gorilax: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	albatrygon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	chillvark: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	komodith: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	giranium: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	flamyle: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	voltecta: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ostria: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ninjoth: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	herbigator: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	anteros: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gladiaster: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	hyperoach: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	barracoth: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	toados: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	voltarak: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	mosqung: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	flamepion: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	hyenix: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	rhinolite: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+};
