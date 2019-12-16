ca44f7fba5b1bb9875a64ac0609f83a748b9bd32
scoopapa <pccompositor@aim.com>
2019-12-16 13:58:30 -0500

    Update rulesets.js

--- data/rulesets.js
+++ data/rulesets.js
@@ -8,26 +8,7 @@ let BattleFormats = {
 
 	// Rulesets
 	///////////////////////////////////////////////////////////////////
-	abilityclause: {
-		effectType: 'ValidatorRule',
-		name: 'Ability Clause',
-		desc: "Prevents teams from having more than one Pok&eacute;mon with the same ability",
-		onBegin() {
-			this.add('rule', 'Ability Clause: Limit one of each Ability');
-		},
-		onValidateTeam(team, format){
-			/**@type {{[k: string]: true}} */
-			let abilityTable = [];
-			for (const set of team) {
-				if (!abilityTable.includes( set.ability )){
-					abilityTable.push( set.ability );
-				}
-				else {
-					return [`You have two pokemon with the ability ${set.ability}.`];
-				}
-			}
-		},
-	},
+
 	standard: {
 		effectType: 'ValidatorRule',
 		name: 'Standard',
