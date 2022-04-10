498b233b14a4aa19094616eaabbad1c79a60ff85
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2022-04-10 18:34:33 +0700

    Create pokedex.ts

---
+++ data/mods/pokedex.ts
@@ -0,0 +1,21 @@
+export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
+   venusaur: {
+	  	inherit: true,
+      baseStats: {hp: 95, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+   }, 
+   venusaurmega: {
+      inherit: true, 
+      baseStats: {hp: 95, atk: 100, def: 123, spa: 122, spd: 120, spe: 80},
+   },
+   charizard: {
+		  inherit: true,
+      baseStats: {hp: 78, atk: 84, def: 78, spa: 115, spd: 85, spe: 100},
+      abilities: {0: "Blaze", H: "Sheer Force"}
+   }, 
+   charizardmegax: {
+	   	inherit: true,
+      baseStats: {hp: 78, atk: 130, def: 117, spa: 130, spd: 85, spe: 100},
+   },
+   charizardmegay: {
+	   	inherit: true,
+      baseStats: {hp: 78, atk: 104, def: 78, spa: 165, spd: 115, spe: 100},
