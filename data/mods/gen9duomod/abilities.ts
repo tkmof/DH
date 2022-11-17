357529e31f775ffd6c210b35ab287a78c64cb14c
Duo <63012562+DuoM2@users.noreply.github.com>
2022-11-17 11:13:18 -0500

    Update abilities.ts

--- data/mods/gen9duomod/abilities.ts
+++ data/mods/gen9duomod/abilities.ts
@@ -1,1 +1,113 @@
 // test
+
+export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
+	obtrusive: {
+		shortDesc: "Stops the Roulette Wheel while the user is active.",
+		onAnyTryMove(target, source, effect) {
+			if (['roulettespin'].includes(effect.id)) {
+				this.attrLastMove('[still]');
+				this.add('cant', this.effectData.target, 'ability: Obtrusive', effect, '[of] ' + target);
+				return false;
+			}
+		},
+		name: "Obtrusive",
+		rating: 1,
+		num: 9001,
+	},
+	
+	hostabsorb: {
+		onModifyMove(move) {
+			if (!move || !move.flags['contact'] || move.target === 'self') return;
+			if (!move.secondaries) {
+				move.secondaries = [];
+			}
+			move.secondaries.push({
+				chance: 100,
+				volatileStatus: 'leechseed',
+				ability: this.dex.getAbility('hostabsorb'),
+			});
+		},
+		name: "Host Absorb",
+		shortDesc: "Contact moves - 100% chance to Leech Seed.",
+		rating: 2,
+		num: 9002,
+	},
+	
+	poweroutage: {
+		onResidualOrder: 26,
+		onResidualSubOrder: 1,
+		onResidual(pokemon) {
+			if (pokemon.activeTurns) {
+				this.boost({spe: -1});
+			}
+		},
+		name: "Power Outage",
+		shortDesc: "Lowers Speed by 1 each turn.",
+		rating: 4.5,
+		num: 9003,
+	},	
+	
+	blazingspirit: {
+		onStart(pokemon) {
+			let activated = false;
+			for (const target of pokemon.side.foe.active) {
+				if (!target || !this.isAdjacent(target, pokemon)) continue;
+				if (!activated) {
+					this.add('-ability', pokemon, 'Blazing Spirit', 'boost');
+					activated = true;
+				}
+				if (target.volatiles['substitute']) {
+					this.add('-immune', target);
+				} else {
+					this.boost({atk: -1}, target, pokemon, null, true);
+				}
+			}
+		},
+		onUpdate(pokemon) {
+			if (pokemon.status === 'slp') {
+				this.add('-activate', pokemon, 'ability: Vital Spirit');
+				pokemon.cureStatus();
+			}
+		},
+		onSetStatus(status, target, source, effect) {
+			if (status.id !== 'slp') return;
+			if ((effect as Move)?.status) {
+				this.add('-immune', target, '[from] ability: Vital Spirit');
+			}
+			return false;
+		},
+		onDamage(damage, target, source, effect) {
+			if (effect.effectType !== 'Move') {
+				if (effect.effectType === 'Ability') this.add('-activate', source, 'ability: ' + effect.name);
+				return false;
+			}
+		},
+		name: "Blazing Spirit",
+		shortDesc: "Combines Vital Spirit, Intimidate, and Magic Guard.",
+		rating: 3.5,
+		num: 9004,
+	},	
+	
+	
+	
+	
+	vent: {
+		onAfterMoveSecondary(target, source, move) {
+			if (!source || source === target || !target.hp || !move.totalDamage) return;
+			const lastAttackedBy = target.getLastAttackedBy();
+			if (!lastAttackedBy) return;
+			const damage = move.multihit ? move.totalDamage : lastAttackedBy.damage;
+			if (target.hp <= target.maxhp / 10 && target.hp + damage > target.maxhp / 10) {
+				this.add('-message', target.name + " is gonna Vent!");
+				target.switchFlag = true;
+				this.heal(target.baseMaxhp);
+			}
+		},
+		name: "Vent",
+		rating: 5,
+		num: 10000,
+	},
+
+	
+	
+};
