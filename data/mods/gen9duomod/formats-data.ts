688799168e978bedf2dcfa3fbfebcc44066fbe0d
Duo <63012562+DuoM2@users.noreply.github.com>
2022-11-17 18:14:52 -0500

    Create formats-data.ts

---
+++ data/mods/gen9duomod/formats-data.ts
@@ -0,0 +1,134 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+    baloon: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    fluxtapestereo: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    pokat: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    spirox: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    badgearth: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    bittle: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    fairydisc: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    shroominesce: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    abysseil: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    draxplosion: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    jewelode: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    treemu: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    capsaken: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    cephalopire: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    chemiclysm: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    commanto: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    eneryth: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    falcola: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    gelsius: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    hydread: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    mountough: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    sanbatter: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    antestar: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    escarglace: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    fauxster: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    gargitect: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    solisle: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    kuribandit: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    mantistorm: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    noxinobi: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    impsaustor: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    gelsiussubzero: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    gelsiushundred: {
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+};
