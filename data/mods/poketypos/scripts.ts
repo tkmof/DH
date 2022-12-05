1f761331d5e606b6500bf6df66e408c48bca4004
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2022-12-06 00:05:46 +0700

    Create scripts.ts

---
+++ data/mods/poketypos/scripts.ts
@@ -0,0 +1,7 @@
+export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
+	teambuilderConfig: {
+		excludeStandardTiers: true,
+		customTiers: ['Paleomons', 'Paleomons NFE', 'Paleomons LC'],
+		customDoublesTiers: ['Paleomons', 'Paleomons NFE', 'Paleomons LC'],
+	},
+};
