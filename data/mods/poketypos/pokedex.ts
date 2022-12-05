d2f05c083f9cbc489899c384b9c83761b1722e35
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2022-12-05 20:08:10 +0700

    Create pokedex.ts

---
+++ data/mods/poketypos/pokedex.ts
@@ -0,0 +1,13 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	 soaperior: {
+		  num: 1,
+	   	name: "Soaperior",
+	  	types: ["Water", "Fairy"],
+	  	baseStats: {hp: 75, atk: 65, def: 95, spa: 65, spd: 95, spe: 133},
+	  	abilities: {0: "Water Veil", H: "Water Bubble"},
+	  	weightkg: 41,
+ 	 },
+   starizard: {
+      num: 2,
+      name: "Starizard",
+          
