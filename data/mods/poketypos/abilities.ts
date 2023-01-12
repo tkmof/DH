9f58f635738683f612bdedd6ce68c5b1734cd611
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-01-12 11:09:35 +0700

    Create abilities.ts

---
+++ data/mods/poketypos/abilities.ts
@@ -0,0 +1,67 @@
+/*
+Ratings and how they work:
+-1: Detrimental
+	  An ability that severely harms the user.
+	ex. Defeatist, Slow Start
+ 0: Useless
+	  An ability with no overall benefit in a singles battle.
+	ex. Color Change, Plus
+ 1: Ineffective
+	  An ability that has minimal effect or is only useful in niche situations.
+	ex. Light Metal, Suction Cups
+ 2: Useful
+	  An ability that can be generally useful.
+	ex. Flame Body, Overcoat
+ 3: Effective
+	  An ability with a strong effect on the user or foe.
+	ex. Chlorophyll, Sturdy
+ 4: Very useful
+	  One of the more popular abilities. It requires minimal support to be effective.
+	ex. Adaptability, Magic Bounce
+ 5: Essential
+	  The sort of ability that defines metagames.
+	ex. Imposter, Shadow Tag
+*/
+
+export const Abilities: {[abilityid: string]: AbilityData} = {
+	thermalexchange: {
+		onDamagingHit(damage, target, source, move) {
+			if (move.type === 'Fire') {
+				this.boost({atk: 1});
+			}
+		},
+		onUpdate(pokemon) {
+			if (pokemon.status === 'brn') {
+				this.add('-activate', pokemon, 'ability: Thermal Exchange');
+				pokemon.cureStatus();
+			}
+		},
+		onSetStatus(status, target, source, effect) {
+			if (status.id !== 'brn') return;
+			if ((effect as Move)?.status) {
+				this.add('-immune', target, '[from] ability: Thermal Exchange');
+			}
+			return false;
+		},
+		name: "Thermal Exchange",
+		rating: 3,
+		num: 300,
+	},
+  guarddog: {
+		onDragOutPriority: 1,
+		onDragOut(pokemon) {
+			this.add('-activate', pokemon, 'ability: Guard Dog');
+			return null;
+			}
+		},
+		onBoost(boost, target, source, effect) {
+			if (effect.id === 'intimidate') {
+				this.boost({atk: 1});
+				this.add('-immune', target, '[from] ability: Guard Dog');
+			}
+		},
+    name: "Guard Dog",
+    rating: 2,
+    num: 301,
+    },
+};
