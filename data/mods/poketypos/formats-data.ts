d9db45cae8cdbc41596892ad1a81a1bf40d7f109
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-01-13 11:22:19 +0700

    Update formats-data.ts

--- data/mods/poketypos/formats-data.ts
+++ data/mods/poketypos/formats-data.ts
@@ -11,4 +11,40 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
       tier: "Paleomons",
       doublesTier: "Paleomons", 
    }, 
+	sapras: {
+		tier: "Paleomons",
+      doublesTier: "Paleomons", 
+   }, 
+	furant: {
+		tier: "Paleomons",
+      doublesTier: "Paleomons", 
+   }, 
+	urthwyrm: {
+		tier: "Paleomons",
+      doublesTier: "Paleomons", 
+   }, 
+	mud: {
+		tier: "Paleomons",
+      doublesTier: "Paleomons", 
+   }, 
+	fweezing: {
+		tier: "Paleomons",
+      doublesTier: "Paleomons", 
+   }, 
+	arcamine: {
+		tier: "Paleomons",
+      doublesTier: "Paleomons", 
+   }, 
+	aurodactyl: {
+		tier: "Paleomons",
+      doublesTier: "Paleomons", 
+   }, 
+	eelix: {
+		tier: "Paleomons",
+      doublesTier: "Paleomons", 
+   }, 
+	appleton: {
+		tier: "Paleomons",
+      doublesTier: "Paleomons", 
+   }, 
 };
