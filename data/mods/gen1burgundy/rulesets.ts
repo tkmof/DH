3b3d862001a72015b814dad344a01a5841e4f2a4
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-01-13 14:11:46 -0500

    Update rulesets.ts

--- data/mods/gen1burgundy/rulesets.ts
+++ data/mods/gen1burgundy/rulesets.ts
@@ -8,6 +8,6 @@
 	standardwithdigandfly: {
 		effectType: 'ValidatorRule',
 		name: 'Standard With Dig and Fly',
-		ruleset: ['Obtainable', 'Desync Clause Mod', 'Sleep Clause Mod', 'Freeze Clause Mod', 'Species Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
+		ruleset: ['Obtainable', 'Desync Clause Mod', 'Stadium Sleep Clause', 'Freeze Clause Mod', 'Species Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
 	},
 };
