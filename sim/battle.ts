35bb6d4119751848e33dcc025b6478fde14eabc8
Anders Hoffman <pccompositor@aim.com>
2023-02-13 06:53:42 -0600

    Update battle.ts

--- sim/battle.ts
+++ sim/battle.ts
@@ -2434,6 +2434,10 @@ export class Battle {
 					this.runEvent('BeforeFaint', pokemon, faintData.source, faintData.effect)) {
 				this.add('faint', pokemon);
 				pokemon.side.pokemonLeft--;
+				// Gen 9 stuff
+					if (!pokemon.side.totalFainted) pokemon.side.totalFainted = 0;
+					if (pokemon.side.totalFainted < 100) pokemon.side.totalFainted++;
+				//
 				this.runEvent('Faint', pokemon, faintData.source, faintData.effect);
 				this.singleEvent('End', pokemon.getAbility(), pokemon.abilityData, pokemon);
 				pokemon.clearVolatile(false);
