af245171967f1464f6e712134f1f384f00d5527e
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-02-14 21:53:55 -0500

    Update scripts.ts

--- data/mods/gen6megasrevisited/scripts.ts
+++ data/mods/gen6megasrevisited/scripts.ts
@@ -1,5 +1,17 @@
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'gen6',
+	pokemon: {
+		// for neutralizing gas
+		ignoringAbility() {
+			if (this.battle.gen >= 5 && !this.isActive) return true;
+			if (this.getAbility().isPermanent) return false;
+			if (this.volatiles['gastroacid']) return true;
+
+			if (this.hasItem('Ability Shield') || this.ability === ('neutralizinggas' as ID)) return false;
+			if (this.volatiles['neutralizinggas']) return true;
+
+			return false;
+		},
 	init: function () {
 		this.modData("Learnsets", "lucario").learnset.meteormash = ["6L1"];
 		this.modData("Learnsets", "lucario").learnset.machpunch = ["6L1"];
