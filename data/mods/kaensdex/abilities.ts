eb85dabd9fe0653563f85c850339317c71db0161
Kaen <basdez@hotmail.es>
2023-02-20 11:42:12 -0300

    Pentamelops new ability

--- data/mods/kaensdex/abilities.ts
+++ data/mods/kaensdex/abilities.ts
@@ -570,6 +570,27 @@ guardianoftheruins: {
 		rating: 2,
 		num: 10031,
 	},
+	
+greatshield: {
+		onCriticalHit: false,
+		onTryHit(pokemon, target, move) {
+			if (move.flags['bullet']) {
+				this.add('-immune', pokemon, '[from] ability: Great Shield');
+				return null;
+			}
+		},
+		onDamagingHitOrder: 1,
+		onDamagingHit(damage, target, source, move) {
+			if (move.flags['contact']) {
+				this.damage(source.baseMaxhp / 8, source, target);
+			}
+		},
+		name: "Great Shield",
+		desc: "Immune to Bullets and can't be crit. Deals 1/8 on contact.",
+		rating: 3,
+		num: 10032,
+	},
+	
 	//gen 9 stuff
 	sharpness: {
 		onBasePowerPriority: 19,
