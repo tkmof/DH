9fcf2f3b71e83fd75ef8691f69f205a77e099212
Anders Hoffman <pccompositor@aim.com>
2023-03-06 03:16:49 -0600

    Update pokedex.ts

--- data/mods/scootopia/pokedex.ts
+++ data/mods/scootopia/pokedex.ts
@@ -320,7 +320,7 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	},
 	cellsius: {
 		num: 1045,
-		name: "cellsius",
+		name: "Cellsius",
 		types: ["Water", "Fairy"],
 		baseStats: {hp: 71, atk: 83, def: 83, spa: 109, spd: 127, spe: 97},
 		abilities: {0: "Beast Boost"},
