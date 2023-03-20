529be164ecefa6ed5398846391848d9220aaa193
Rasdanation <Rasdanation@gmail.com>
2023-03-20 19:04:07 -0400

    Create formats-data.ts

---
+++ data/mods/gen9crossoverchaos/formats-data.ts
@@ -0,0 +1,18 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+  malenia: {
+		tier: "CCOU",
+		doublesTier: "DOU",
+	},
+	kingdedede: {
+		tier: "CCOU",
+		doublesTier: "DOU",
+	},
+	maskeddedede: {
+		tier: "CCUbers",
+		doublesTier: "DOU",
+	},
+	zelda: {
+		tier: "CCOU",
+		doublesTier: "DOU",
+	},
+};
