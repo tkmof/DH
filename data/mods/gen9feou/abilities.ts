4ed1ed25057fafa65222b9eb2ea018231428d0d8
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-03-27 12:44:45 -0400

    Update abilities.ts

--- data/mods/gen9feou/abilities.ts
+++ data/mods/gen9feou/abilities.ts
@@ -164,4 +164,328 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		},
 	  name: "Growth Spurt",
     },
+	lightdrive: {
+	  shortDesc: "(Partially functional) Light Metal + Quark Drive. Quark Drive activates if the user is lighter.",
+		onModifyWeight(weighthg) {
+			return this.trunc(weighthg / 2);
+		},
+		onStart(pokemon) {
+			this.singleEvent('TerrainChange', this.effect, this.effectData, pokemon);
+		},
+		onUpdate(pokemon) {
+			// if (pokemon.transformed) return;
+			// Protosynthesis is not affected by Utility Umbrella
+			if (this.field.isTerrain('electricterrain') && !pokemon.volatiles['lightdrive']) {
+				pokemon.addVolatile('lightdrive');
+			} else if (pokemon.hasItem('lightdrive') && !this.field.isTerrain('electricterrain') && pokemon.useItem()) {
+				pokemon.removeVolatile('lightdrive');
+				pokemon.addVolatile('lightdrive', pokemon, Dex.getItem('boosterenergy'));
+				pokemon.volatiles['lightdrive'].fromBooster = true;
+			} else if (!pokemon.volatiles['lightdrive']?.fromBooster && !this.field.isTerrain('electricterrain')) {
+				pokemon.removeVolatile('lightdrive');
+			}
+		},
+		onEnd(pokemon) {
+			delete pokemon.volatiles['lightdrive'];
+			this.add('-end', pokemon, 'Light Drive', '[silent]');
+		},
+		condition: {
+			noCopy: true,
+			onStart(pokemon, source, effect) {
+				if (effect?.id === 'boosterenergy') {
+					this.effectData.fromBooster = true;
+					this.add('-activate', pokemon, 'ability: Light Drive', '[fromitem]');
+				} else {
+					this.add('-activate', pokemon, 'ability: Light Drive');
+				}
+				this.effectData.bestStat = pokemon.getBestStat(false, true);
+				this.add('-start', pokemon, 'lightdrive' + this.effectData.bestStat);
+			},
+			onModifyAtkPriority: 5,
+			onModifyAtk(atk, source, target, move) {
+				if (this.effectData.bestStat !== 'atk') return;
+				this.debug('Light Drive atk boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifyDefPriority: 6,
+			onModifyDef(def, target, source, move) {
+				if (this.effectData.bestStat !== 'def') return;
+				this.debug('Light Drive def boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifySpAPriority: 5,
+			onModifySpA(relayVar, source, target, move) {
+				if (this.effectData.bestStat !== 'spa') return;
+				this.debug('Light Drive spa boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifySpDPriority: 6,
+			onModifySpD(relayVar, target, source, move) {
+				if (this.effectData.bestStat !== 'spd') return;
+				this.debug('Light Drive spd boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifySpe(spe, pokemon) {
+				if (this.effectData.bestStat !== 'spe') return;
+				this.debug('Light Drive spe boost');
+				return this.chainModify(1.5);
+			},
+			onEnd(pokemon) {
+				this.add('-end', pokemon, 'Light Drive');
+			},
+		},
+		isPermanent: true,
+		name: "Light Drive",
+		rating: 1,
+		num: 135,
+	},
+	scraprock: {
+	  shortDesc: "Scrappy + Solid Rock",
+		onSourceModifyDamage(damage, source, target, move) {
+			if (target.getMoveHitData(move).typeMod > 0) {
+				this.debug('Scrap Rock neutralize');
+				return this.chainModify(0.75);
+			}
+		},
+		onModifyMovePriority: -5,
+		onModifyMove(move) {
+			if (!move.ignoreImmunity) move.ignoreImmunity = {};
+			if (move.ignoreImmunity !== true) {
+				move.ignoreImmunity['Fighting'] = true;
+				move.ignoreImmunity['Normal'] = true;
+			}
+		},
+		onBoost(boost, target, source, effect) {
+			if (effect.id === 'intimidate' || effect.id === 'forestfury') {
+				delete boost.atk;
+				this.add('-immune', target, '[from] ability: Scrap Rock');
+			}
+		},
+		name: "Scrap Rock",
+		rating: 3,
+	},
+	reachless: {
+	  shortDesc: "Effects of Rock Head and Reckless.",
+		onBasePowerPriority: 23,
+		onBasePower(basePower, attacker, defender, move) {
+			if (move.recoil || move.hasCrashDamage) {
+				this.debug('Reachless boost');
+				return this.chainModify([0x1333, 0x1000]);
+			}
+		},
+		onDamage(damage, target, source, effect) {
+			if (effect.id === 'recoil') {
+				if (!this.activeMove) throw new Error("Battle.activeMove is null");
+				if (this.activeMove.id !== 'struggle') return null;
+			}
+		},
+		name: "Reachless",
+		rating: 3,
+	},
+	openingact: {
+	  shortDesc: "Protosynthesis + Magician",
+		onSourceHit(target, source, move) {
+			if (!move || !target) return;
+			if (target !== source && move.category !== 'Status') {
+				if (source.item || source.volatiles['gem'] || move.id === 'fling') return;
+				const yourItem = target.takeItem(source);
+				if (!yourItem) return;
+				if (!source.setItem(yourItem)) {
+					target.item = yourItem.id; // bypass setItem so we don't break choicelock or anything
+					return;
+				}
+				this.add('-item', source, yourItem, '[from] ability: Opening Act', '[of] ' + target);
+			}
+		},
+		onStart(pokemon) {
+			this.singleEvent('WeatherChange', this.effect, this.effectData, pokemon);
+		},
+		onUpdate(pokemon) {
+			// if (pokemon.transformed) return;
+			// Opening Act is not affected by Utility Umbrella
+			if (this.field.isWeather('sunnyday') && !pokemon.volatiles['openingact']) {
+				pokemon.addVolatile('openingact');
+			} else if (pokemon.hasItem('boosterenergy') && !this.field.isWeather('sunnyday') && pokemon.useItem()) {
+				pokemon.removeVolatile('openingact');
+				pokemon.addVolatile('openingact', pokemon, Dex.getItem('boosterenergy'));
+				pokemon.volatiles['openingact'].fromBooster = true;
+			} else if (!pokemon.volatiles['openingact']?.fromBooster && !this.field.isWeather('sunnyday')) {
+				pokemon.removeVolatile('openingact');
+			}
+		},
+		onEnd(pokemon) {
+			delete pokemon.volatiles['openingact'];
+			this.add('-end', pokemon, 'Opening Act', '[silent]');
+		},
+		condition: {
+			noCopy: true,
+			onStart(pokemon, source, effect) {
+				if (effect?.id === 'boosterenergy') {
+					this.effectData.fromBooster = true;
+					this.add('-activate', pokemon, 'ability: Opening Act', '[fromitem]');
+				} else {
+					this.add('-activate', pokemon, 'ability: Opening Act');
+				}
+				this.effectData.bestStat = pokemon.getBestStat(false, true);
+				this.add('-start', pokemon, 'openingact' + this.effectData.bestStat);
+			},
+			onModifyAtkPriority: 5,
+			onModifyAtk(atk, source, target, move) {
+				if (this.effectData.bestStat !== 'atk') return;
+				this.debug('Opening Act atk boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifyDefPriority: 6,
+			onModifyDef(def, target, source, move) {
+				if (this.effectData.bestStat !== 'def') return;
+				this.debug('Opening Act def boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifySpAPriority: 5,
+			onModifySpA(relayVar, source, target, move) {
+				if (this.effectData.bestStat !== 'spa') return;
+				this.debug('Opening Act spa boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifySpDPriority: 6,
+			onModifySpD(relayVar, target, source, move) {
+				if (this.effectData.bestStat !== 'spd') return;
+				this.debug('Opening Act spd boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifySpe(spe, pokemon) {
+				if (this.effectData.bestStat !== 'spe') return;
+				this.debug('Opening Act spe boost');
+				return this.chainModify(1.5);
+			},
+			onEnd(pokemon) {
+				this.add('-end', pokemon, 'Opening Act');
+			},
+		},
+		isPermanent: true,
+		name: "Opening Act",
+		rating: 3,
+	},
+	necromancer: {
+	  shortDesc: "This Pokemon's offensive stat is multiplied by 1.5 while using a Ghost-type attack; can't be statused.",
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Ghost') {
+				this.debug('Necromancer boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Ghost') {
+				this.debug('Necromancer boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onSetStatus(status, target, source, effect) {
+			if ((effect as Move)?.status) {
+				this.add('-immune', target, '[from] ability: Necromancer');
+			}
+			return false;
+		},
+		onTryAddVolatile(status, target) {
+			if (status.id === 'yawn') {
+				this.add('-immune', target, '[from] ability: Necromancer');
+				return null;
+			}
+		},
+		name: "Necromancer",
+		rating: 3,
+	},
+	regainpatience: {
+	  shortDesc: "Berserk + Regenerator",
+		onAfterMoveSecondary(target, source, move) {
+			if (!source || source === target || !target.hp || !move.totalDamage) return;
+			const lastAttackedBy = target.getLastAttackedBy();
+			if (!lastAttackedBy) return;
+			const damage = move.multihit ? move.totalDamage : lastAttackedBy.damage;
+			if (target.hp <= target.maxhp / 2 && target.hp + damage > target.maxhp / 2) {
+				this.boost({spa: 1});
+			}
+		},
+		onSwitchOut(pokemon) {
+			pokemon.heal(pokemon.baseMaxhp / 3);
+		},
+		name: "Regain Patience",
+		rating: 3,
+	},
+	quarksurge: {
+	  shortDesc: "Quark Drive + Electric Surge",
+		onStart(source) {
+			this.field.setTerrain('electricterrain');
+		},
+		onStart(pokemon) {
+			this.singleEvent('TerrainChange', this.effect, this.effectData, pokemon);
+		},
+		onUpdate(pokemon) {
+			// if (pokemon.transformed) return;
+			// Protosynthesis is not affected by Utility Umbrella
+			if (this.field.isTerrain('electricterrain') && !pokemon.volatiles['quarksurge']) {
+				pokemon.addVolatile('quarksurge');
+			} else if (pokemon.hasItem('quarksurge') && !this.field.isTerrain('electricterrain') && pokemon.useItem()) {
+				pokemon.removeVolatile('quarksurge');
+				pokemon.addVolatile('quarksurge', pokemon, Dex.getItem('boosterenergy'));
+				pokemon.volatiles['quarksurge'].fromBooster = true;
+			} else if (!pokemon.volatiles['quarksurge']?.fromBooster && !this.field.isTerrain('electricterrain')) {
+				pokemon.removeVolatile('quarksurge');
+			}
+		},
+		onEnd(pokemon) {
+			delete pokemon.volatiles['quarksurge'];
+			this.add('-end', pokemon, 'Quark Surge', '[silent]');
+		},
+		condition: {
+			noCopy: true,
+			onStart(pokemon, source, effect) {
+				if (effect?.id === 'boosterenergy') {
+					this.effectData.fromBooster = true;
+					this.add('-activate', pokemon, 'ability: Quark Surge', '[fromitem]');
+				} else {
+					this.add('-activate', pokemon, 'ability: Quark Surge');
+				}
+				this.effectData.bestStat = pokemon.getBestStat(false, true);
+				this.add('-start', pokemon, 'quarksurge' + this.effectData.bestStat);
+			},
+			onModifyAtkPriority: 5,
+			onModifyAtk(atk, source, target, move) {
+				if (this.effectData.bestStat !== 'atk') return;
+				this.debug('Quark Surge atk boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifyDefPriority: 6,
+			onModifyDef(def, target, source, move) {
+				if (this.effectData.bestStat !== 'def') return;
+				this.debug('Quark Surge def boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifySpAPriority: 5,
+			onModifySpA(relayVar, source, target, move) {
+				if (this.effectData.bestStat !== 'spa') return;
+				this.debug('Quark Surge spa boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifySpDPriority: 6,
+			onModifySpD(relayVar, target, source, move) {
+				if (this.effectData.bestStat !== 'spd') return;
+				this.debug('Quark Surge spd boost');
+				return this.chainModify([5325, 4096]);
+			},
+			onModifySpe(spe, pokemon) {
+				if (this.effectData.bestStat !== 'spe') return;
+				this.debug('Quark Surge spe boost');
+				return this.chainModify(1.5);
+			},
+			onEnd(pokemon) {
+				this.add('-end', pokemon, 'Quark Surge');
+			},
+		},
+		isPermanent: true,
+		name: "Quark Surge",
+		rating: 3,
+	},
 };
