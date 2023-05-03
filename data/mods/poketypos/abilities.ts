e85ffd58e3ade34439aabafa1d6d109ed65720ad
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-05-03 20:46:36 +0700

    Update abilities.ts

--- data/mods/poketypos/abilities.ts
+++ data/mods/poketypos/abilities.ts
@@ -247,4 +247,12 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		rating: 2,
 		num: 298,
 	},
+	honeyfists: {
+		onModifyMove(move, pokemon) {
+			if(pokemon.types.includes(move.type)) move.drain = [1, 8];
+		},
+		name: "Honey Fists",
+		shortDesc: "When this Pokemon uses a STAB attack, it heals 1/8 of the damage dealt.",
+		rating: 4,
+	},
 };
