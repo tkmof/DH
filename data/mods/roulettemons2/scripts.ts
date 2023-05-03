dad1be9683c205feccb208fb56d04c674229fe57
Duo <63012562+DuoM2@users.noreply.github.com>
2023-05-03 15:25:07 -0400

    Create scripts.ts

---
+++ data/mods/roulettemons2/scripts.ts
@@ -0,0 +1,7 @@
+export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
+	teambuilderConfig: {
+		excludeStandardTiers: true,
+		customTiers: ['R2OU, R2Ubers'],
+		customDoublesTiers: ['R2OU, R2Ubers'],
+	},
+};
