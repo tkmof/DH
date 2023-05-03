a0f7b49eb64109ce2c22e7fe338dc52417d67561
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-05-03 21:57:44 +0700

    Create scripts.ts

---
+++ data/mods/regeneration/scripts.ts
@@ -0,0 +1,7 @@
+export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
+	teambuilderConfig: {
+		excludeStandardTiers: true,
+		customTiers: ['ReGeneration', 'ReGeneration NFE', 'ReGeneration LC'],
+		customDoublesTiers: ['ReGeneration', 'ReGeneration NFE', 'ReGeneration LC'],
+	},
+};
