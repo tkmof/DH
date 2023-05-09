d2cf13deb8680e62dd6587e90b36f2143a0313be
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-05-08 23:07:20 -0400

    Update scripts.ts

--- data/mods/gen6megasrevisited/scripts.ts
+++ data/mods/gen6megasrevisited/scripts.ts
@@ -120,5 +120,8 @@ export const Scripts: ModdedBattleScriptsData = {
 		this.modData("Learnsets", "salamence").learnset.hurricane = ["6L1"];
 		this.modData("Learnsets", "salamence").learnset.airslash = ["6L1"];
 		this.modData("Learnsets", "salamence").learnset.ironhead = ["6L1"];
+		this.modData("Learnsets", "tyranitar").learnset.wildcharge = ["6L1"];
+		this.modData("Learnsets", "tyranitar").learnset.iciclecrash = ["6L1"];
+		this.modData("Learnsets", "tyranitar").learnset.waterfall = ["6L1"];
 	},
 };
