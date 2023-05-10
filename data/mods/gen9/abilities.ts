b4ae95296d3fc9a11a2c79bf12f2c07b70db6e8f
scoopapa <pccompositor@aim.com>
2023-05-10 17:36:26 -0500

    Update abilities.ts

--- data/mods/gen9/abilities.ts
+++ data/mods/gen9/abilities.ts
@@ -349,6 +349,33 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		rating: 3,
 		num: 290,
 	},
+	orichalcumpulse: {
+		onStart(pokemon) {
+			if (
+				!this.field.setWeather('sunnyday') &&
+				['sunnyday', 'desolateland'].includes(pokemon.effectiveWeather())
+			) {
+				this.add('-activate', pokemon, 'ability: Orichalcum Pulse');
+			}
+		},
+		onAnyWeatherStart(target, source) {
+			const pokemon = this.effectData.target;
+			if (pokemon === source) return;
+			if (['sunnyday', 'desolateland'].includes(pokemon.effectiveWeather())) {
+				this.add('-activate', pokemon, 'ability: Orichalcum Pulse');
+			}
+		},
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, pokemon) {
+			if (['sunnyday', 'desolateland'].includes(pokemon.effectiveWeather())) {
+				this.debug('Orichalcum boost');
+				return this.chainModify([5325, 4096]);
+			}
+		},
+		name: "Orichalcum Pulse",
+		rating: 4,
+		num: 288,
+	},
 	protosynthesis: {
 		onStart(pokemon) {
 			this.singleEvent('WeatherChange', this.effect, this.effectData, pokemon);
