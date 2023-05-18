73ddd9f90b2ada22907b988d9c276e354befd14b
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-05-18 18:35:52 +0700

    Update scripts.ts

--- data/mods/regeneration/scripts.ts
+++ data/mods/regeneration/scripts.ts
@@ -81,6 +81,8 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 		}
    },
 	modifyDamage(
+		baseDamage: number, pokemon: Pokemon, target: Pokemon, move: ActiveMove, suppressMessages = false
+	) {
 		if (move.forceSTAB || (type !== '???' && (pokemon.hasType(type) || pokemon.species.teraBoost?.includes(type)))) {
 			// The "???" type never gets STAB
 			// Not even if you Roost in Gen 4 and somehow manage to use
