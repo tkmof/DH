2a78a578e267da87dd396bf6cc16c38402537823
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-06-07 17:14:03 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1395,7 +1395,8 @@ export const Formats: FormatList = [
 		name: "[Gen 1] Glitch OU",
 		mod: 'gen1glitch',
 		ruleset: ['Standard', 'Data Mod'],
-		banlist: ['Uber', 'Agility + Wrap', 'Agility + Fire Spin', 'Agility + Bind', 'Agility + Clamp', 'Amnesia', 'Sleep Powder'],
+		banlist: ['Uber', 'Agility + Wrap', 'Agility + Fire Spin', 'Agility + Bind', 'Agility + Clamp', 'Amnesia', 'Sleep Powder'
+					'Self-Destruct', 'Explosion'],
 	},
 	{
 		
