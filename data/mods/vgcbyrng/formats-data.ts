877b64764b2ac67699ca2d01db4c0f5151686ed6
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-06-19 23:19:38 -0400

    Create formats-data.ts

---
+++ data/mods/vgcbyrng/formats-data.ts
@@ -0,0 +1,894 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	whimsicott: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	illumise: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	nidoqueen: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	comfey: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	furret: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	lickilicky: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	tatsugiri: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	passimian: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	parasect: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	slurpuff: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	claydol: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	lucario: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	skeledirge: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	tinglu: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	glalie: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	volcarona: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	froslass: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	pyroar: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	skarmory: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	cacturne: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	wobbuffet: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	ursaluna: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	clawitzer: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	orthworm: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	wigglytuff: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	copperajah: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	wormadam: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	mothim: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	camerupt: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	forretress: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	fearow: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	abomasnow: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	hawlucha: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	wochien: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	enamorus: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	quaquaval: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	garbodor: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	dachsbun: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	golurk: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	houndstone: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	bruxish: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	brutebonnet: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	medicham: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	xatu: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	thievul: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	bouffalant: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	tsareena: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	bellibolt: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	ironbundle: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	politoed: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	dracovish: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	hitmonlee: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	hitmontop: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	poliwrath: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	jumpluff: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	cursola: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	corsola: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	ironmoth: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	latios: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	arboliva: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	infernape: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	audino: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	plusle: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	perrserker: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	tropius: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	veluza: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	persian: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	persianalola: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	barraskewda: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	milotic: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	rhyperior: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	sceptile: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	togedemaru: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	klinklang: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	gliscor: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	oranguru: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	shuckle: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	heatmor: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvally: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	golisopod: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	tapukoko: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	pawmot: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	pyukumuku: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	seismitoad: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	slaking: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	celesteela: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	cloyster: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	eelektross: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	nidoking: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	mamoswine: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	lapras: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	greedent: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	raikou: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	spinda: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	cobalion: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	drapion: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	kingdra: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	tyranitar: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	beautifly: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	dustox: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	klawf: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	corviknight: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	tatsugiristretchy: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	tatsugiricurly: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	enamorustherian: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallybug: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallydark: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallydragon: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallyfairy: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallyfire: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallyflying: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallyelectric: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallygrass: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallyground: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallyice: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallypoison: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallypsychic: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallyrock: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallysteel: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallywater: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallyghost: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	silvallyfighting: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	wormadamsandy: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	wormadamtrash: {
+		tier: "RNG FE",
+		doublesTier: "RNG FE",
+	},
+	nidorina: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	crocalor: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	ursaring: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	jigglytuff: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	quaxwell: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	steenee: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	poliwhirl: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	dolliv: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	skiploom: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	monferno: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	rhydon: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	grovyle: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	klang: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	pawmo: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	vigoroth: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	palpitoad: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	eelektrik: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	piloswine: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	nidorino: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	seadra: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	pupitar: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	silcoon: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	cascoon: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	corvisquire: {
+		tier: "RNG NFE",
+		doublesTier: "RNG NFE",
+	},
+	cottonee: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	sentret: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	lickitung: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	paras: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	swirlix: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	baltoy: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	snorunt: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	larvesta: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	fuecoco: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	riolu: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	litleo: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	cacnea: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	wynaut: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	teddiursa: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	clauncher: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	igglybuff: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	cufant: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	burmy: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	snover: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	pinceo: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	numel: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	spearow: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	golett: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	fidough: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	trubbish: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	quaxly: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	natu: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	greavard: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	meditite: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	nickit: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	bounsweet: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	tadbulb: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	tyrogue: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	poliwag: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	corsolagalar: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	smoliv: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	hoppip: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	chimchar: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	meowth: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	meowthalola: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	meowthgalar: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	treecko: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	rhyhorn: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	feebas: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	arrokuda: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	klink: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	gligar: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	typenull: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	pawmi: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	wimpod: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	tynamo: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	swinub: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	nidoranm: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	nidoranf: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	skwovet: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	skorupi: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	wurmple: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	horsea: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	larvitar: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	rookidee: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	tympole: {
+		tier: "RNG LC",
+		doublesTier: "RNG LC",
+	},
+	yveltal: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	rayquaza: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	dialga: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	dialgaorigin: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	reshiram: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	xerneas: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	lunala: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	lugia: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	zacian: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	zaciancrowned: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	zygarde: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	zygarde10: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	zygardecomplete: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+	miraidon: {
+		tier: "RNG Ubers",
+		doublesTier: "RNG Ubers",
+	},
+};
