da5b834361bda1b69d3912f06f2c99e0001149f0
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2023-06-21 09:59:15 -0700

    set in stone slate 1

--- config/formats.ts
+++ config/formats.ts
@@ -521,6 +521,28 @@ export const Formats: FormatList = [
 		},
 	},
 	{
+		name: "[Gen 9] Set in Stone",
+		desc: [
+			"<b>Set in Stone</b>: A micrometa where Pokemon are customized based on a combination of two player's set ideas.",
+		],
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/set-in-stone-phase-2-slate-2.3722451/post-9648171"> Set in Stone on Smogon Forums</a>`,
+		      ],
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Z-Move Clause', 'Data Mod', 'Mega Data Mod'],
+		banlist: [],
+		onValidateTeam(team, format) {
+			/**@type {{[k: string]: true}} */
+			let speciesTable = {};
+			for (const set of team) {
+				let template = this.dex.getSpecies(set.species);
+				if ( template.tier !== 'SS') {
+					return [set.species + ' is not usable in Triple Threat.'];
+				}
+			}
+		},
+		mod: 'setinstone',
+	},
+	{
 		name: "[Gen 8] Super Smash Stereotypes",
 		desc: [
 			"<b>Super Smash Stereotypes</b>: A project that aims to create a micrometa containing a Pokemon from other mods for all 171 possible types.",
