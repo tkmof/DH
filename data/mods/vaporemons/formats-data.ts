37ddb9b3d401e385c1f5efebe5f421fc68ad520d
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-06-22 04:49:57 -0400

    Update formats-data.ts

--- data/mods/vaporemons/formats-data.ts
+++ data/mods/vaporemons/formats-data.ts
@@ -5534,7 +5534,7 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		tier: "RU",
 		doublesTier: "(DUU)",
 		natDexTier: "RU",
-	},
+	}, /*
 	revavroomcaph: {
 		tier: "RU",
 		doublesTier: "(DUU)",
@@ -5559,7 +5559,7 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		tier: "RU",
 		doublesTier: "(DUU)",
 		natDexTier: "RU",
-	},
+	}, */
 	orthworm: {
 		tier: "UU",
 		doublesTier: "(DUU)",
