10f40a4c686a7664abb5b4bf1f9cebd79fb313b4
Lysio4 <lukas.moncorge@gmail.com>
2023-07-06 16:26:13 +0200

    Adding Tauros

--- data/mods/agoldenexperience/formats-data.ts
+++ data/mods/agoldenexperience/formats-data.ts
@@ -837,6 +837,18 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		tier: "NU",
 		doublesTier: "(DUU)",
 	},
+	taurospaldeacombat: {
+		tier: "NU",
+		doublesTier: "(DUU)",
+	},
+	taurospaldeablaze: {
+		tier: "RU",
+		doublesTier: "(DUU)",
+	},
+	taurospaldeaaqua: {
+		tier: "UU",
+		doublesTier: "(DUU)",
+	},
 	magikarp: {
 		tier: "LC",
 	},
