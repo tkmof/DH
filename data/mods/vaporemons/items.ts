0085879109af3b8129da4e2d6d90e8b16cd3f8aa
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-07-09 16:38:40 -0400

    Update items.ts

--- data/mods/vaporemons/items.ts
+++ data/mods/vaporemons/items.ts
@@ -978,4 +978,72 @@ export const Items: {[k: string]: ModdedItemData} = {
 		gen: 7,
 		desc: "If the terrain is Grassy Terrain, raises holder's Defense by 1 stage. Single use.",
 	},
+	dracoplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	dreadplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	earthplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	fistplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	flameplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	icicleplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	insectplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	ironplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	meadowplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mindplate‎: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	pixieplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	skyplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	splashplate‎: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	spookyplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	stoneplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	toxicplate‎: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	zapplate‎: {
+		inherit: true,
+		isNonstandard: null,
+	},
 };
