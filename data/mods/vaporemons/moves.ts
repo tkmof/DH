bb426c9b4fea249f4e43fe01a9551781202bf670
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-07-11 01:54:35 -0400

    Update moves.ts

--- data/mods/vaporemons/moves.ts
+++ data/mods/vaporemons/moves.ts
@@ -3032,6 +3032,9 @@ stickyweb: {
 					this.add('-activate', source, 'ability: Persistent', effect);
 					return 7;
 				}
+				if (source?.hasAbility('deathaura')) {
+					return 0;
+				}
 				return 5;
 			},
 			onStart(pokemon) {
